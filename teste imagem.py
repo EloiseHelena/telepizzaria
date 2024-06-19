7dc686d67bff95d5f7462df5e6858e12b106e247
Eloise Helena de Moraes Rôla <182300648@senacinformatica.br>
2024-06-18 21:41:12 -0300

    salvando melhorias, faltam os valores unitários

--- teste imagem.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image, ImageTk 
-import tkinter as tk 
-
-root = tk.Tk()
-tkimage = ImageTk.PhotoImage(Image.open("imagens\Pizza-3007395.jpg"))
-tk.Label(root, image=tkimage).pack()
-root.mainloop()
